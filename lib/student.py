2a248f8c35d450de11a580d175b16c2ac57bf83e
Timothy Ryon <tim@timryon.com>
2024-03-30 22:10:54 -0400

    finish teaching and learning and tests pass

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,9 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = list()
+
+    def learn(self, new_information):
+        self.knowledge.append(new_information)
